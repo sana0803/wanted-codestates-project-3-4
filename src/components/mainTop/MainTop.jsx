47a5a369cc9cb09aa849cdd5747197bb1ce715b4
haagmin5382 <itworker5382@gmail.com>
2022-03-10 17:17:22 +0900

    :sparkles: feat : add BannerMessage

---
+++ src/components/mainTop/MainTop.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BannerMessage from '../BannerMessage';
+
+const MainTop = () => {
+  return (
+    <div>
+      {/* BannerMessage component 사용예시 */}
+      <BannerMessage text="여기에 적으면 내용이 나옵니다" />
+    </div>
+  );
+};
+
+export default MainTop;
