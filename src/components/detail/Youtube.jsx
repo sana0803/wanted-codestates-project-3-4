8756201bc5faa5ee27841ea2e1d9f8accf867fb9
khw970421 <khw970421@kakao.com>
2022-03-10 22:48:05 +0900

    ✨ feat : youtube iframe 삽입 성공

--- src/components/detail/Youtube.jsx
+++ src/components/detail/Youtube.jsx
@@ -9,7 +9,7 @@ const DUMMY_DATA = {
   body: '웹 3.0은 무엇일까요? 읽기만 가능했던 1세대, 읽기와 쓰기가 모두 가능했던 2세대를 지나 이제 인터넷이 3세대로 진입하는 중이라는데? 블록체인과 암호화폐 산업의 발전과 맞물려 급부상 중인 웹 3.0의 현 주소와 미래를 확인해보세요!',
   image:
     'https://sandbank-image.s3.ap-northeast-2.amazonaws.com/info/block_30.png',
-  link: 'tnJwx53uLEU',
+  link: 'twm70X8_PxI',
   upload_date: '2022-01-20',
   like_cnt: 0,
   like_top: 0,
@@ -18,6 +18,13 @@ const Youtube = () => {
   return (
     <YoutubeContainer>
       <Banner title={'블록체인 NOW'} />
+      <iframe
+        width="560"
+        height="315"
+        src={'https://www.youtube.com/embed/' + DUMMY_DATA.link}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      ></iframe>
       <TitleContainer title={DUMMY_DATA.title} />
       <Body>{DUMMY_DATA.body}</Body>
     </YoutubeContainer>
@@ -26,6 +33,7 @@ const Youtube = () => {
 
 const YoutubeContainer = styled.div`
   width: 720px;
+  margin: 0 auto;
 `;
 
 const Body = styled.div`
